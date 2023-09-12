deee7834c531df7c7d95fd6715bea8b957de3445
Nimish Agrawal <realnimish@gmail.com>
2023-09-12 11:47:25 +0530

    feat: remove `simple-contract` example

--- contracts/common/lib.rs
+++ contracts/common/lib.rs
@@ -1,9 +1,6 @@
 #![cfg_attr(not(feature = "std"), no_std, no_main)]
 
 pub mod call_encodings;
-mod simple_contract_error;
 pub mod xcm_utils;
 
-pub use simple_contract_error::SimpleContractError;
 pub use sp_weights::Weight;
-pub type MultiAddress = ink::primitives::AccountId;
